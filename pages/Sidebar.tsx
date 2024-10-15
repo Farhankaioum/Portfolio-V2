ba120293cc3646ccf98b7ef15d4de16f0aad3e24
Farhankaioum <keiuom.swe@gmail.com>
2024-10-15 22:41:08 +1100

    Blog button title changed

--- pages/Sidebar.tsx
+++ pages/Sidebar.tsx
@@ -52,7 +52,7 @@ const Header = () => {
               >
                 <path d="M8.5 14.5A2.5 2.5 0 0 0 11 12c0-1.38-.5-2-1-3-1.072-2.143-.224-4.054 2-6 .5 2.5 2 4.9 4 6.5 2 1.6 3 3.5 3 5.5a7 7 0 1 1-14 0c0-1.153.433-2.294 1-3a2.5 2.5 0 0 0 2.5 2.5z"></path>
               </svg>
-              My CP Blog
+              My SE Blog
             </a>
             <a
               className="flex items-center rounded uppercase bg-teal-300 px-4 py-2 text-xs text-teal-900 hover:text-teal-100 hover:bg-teal-400/10 font-medium"
